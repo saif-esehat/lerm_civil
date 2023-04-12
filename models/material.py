d81f48014f48171e88d70ccac5237a73e4fb9e3c
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2023-04-12 11:16:38 +0000

    material

--- models/material.py
+++ models/material.py
@@ -5,23 +5,29 @@ class Material(models.Model):
 
     is_sample = fields.Boolean(string="Is Sample?")
 
-    test_parameter = fields.Char("Test Parameters")
-    results = fields.Char("Results")
-    specifications = fields.Char("Specifications")
-    unit = fields.Char("Unit")
-    method_reference = fields.Char("Method Reference")
-    result_remark = fields.Char("Results Remark")
+    test_parameter = fields.Char("Test Column Title")
+    results = fields.Char("Result Column Title")
+    specifications = fields.Char("Specifications Column Title")
+    unit = fields.Char("Unit Column Title")
+    method_reference = fields.Char("Method Reference Column Title")
+    result_remark = fields.Char("Results Remark Column Title")
     discipline = fields.Many2one('lerm_civil.discipline',string="Discipline")
     group = fields.Many2one('lerm_civil.group',string="Group")
     test_format_no = fields.Char(string="Test Format No")
     data_sheet_format_no = fields.Char(string="Data Sheet Format No")
+    group_ids = fields.Many2many('lerm_civil.group',string="Group Ids",compute="compute_group_ids")
 
     parameter_table = fields.One2many('lerm.parameter.line','parameter_id',string="Parameter")
     size_table = fields.One2many('lerm.size.line','size_id',string="Size")
     qty_table = fields.One2many('lerm.qty.line','qty_id',string="Qty")
     grade_table = fields.One2many('lerm.grade.line','grade_id',string="Grade")
 
-
+    @api.depends('discipline')
+    def compute_group_ids(self):
+        for record in self:
+            record.group = ''
+            group_ids = self.env['lerm_civil.group'].search([('discipline', '=', record.discipline.id)])
+            record.group_ids = group_ids
 
 class ParameterLine(models.Model):
     _name = 'lerm.parameter.line'
