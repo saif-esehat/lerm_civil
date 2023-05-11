1b57e36a96dc53771a7c5819efa7083ef3c9d61e
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2023-05-11 11:52:58 +0000

    nabl

--- models/parameter_master.py
+++ models/parameter_master.py
@@ -15,5 +15,6 @@ class ParameterMaster(models.Model):
     unit = fields.Char("Unit")
     test_method = fields.Many2one('lerm_civil.test_method',string="Test Method")
     discipline = fields.Many2one('lerm_civil.discipline',string="Discipline")
+    nabl_select = fields.Selection([('nabl', 'NABL'), ('non_nabl', 'Non NABL')], string='NABL')
     
 
