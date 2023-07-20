52ff4f0aff1ec75003051c01e1223f2d4df8fbb6
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2023-07-20 14:54:29 +0000

    coupler & split tensile

--- models/__init__.py
+++ models/__init__.py
@@ -10,16 +10,15 @@ from . import srf_report
 from . import sample_report
 from . import contractor
 from . import data_sheets_report
-<<<<<<< HEAD
 from .mechanical import sieve_analysis
+from .mechanical import split_tensile_strength
+from .mechanical import coupler
 from .ndt import crackdepth
 from .ndt import acilcrackdepth
 from .ndt import covermeter
 from .ndt import carbonation
 from .ndt import rebound_hammer
 from .ndt import concrete_core
-=======
 from .ndt import crackdepth
 from .ndt import acil_upv
 from .ndt import upv
->>>>>>> 8c8effcf1e599499a2612571b8bade20dc071c95
