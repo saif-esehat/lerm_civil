0b8c50f49b71f6f54cb2fd88307064d11819e3c0
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2023-07-21 17:55:33 +0000

    soundness and sieve srno

--- models/mechanical/sieve_analysis.py
+++ models/mechanical/sieve_analysis.py
@@ -42,13 +42,57 @@ class SieveAnalysisLine(models.Model):
     _name = "mechanical.sieve.analysis.line"
     parent_id = fields.Many2one('mechanical.sieve.analysis', string="Parent Id")
     
+    serial_no = fields.Integer(string="Sr. No", readonly=True, copy=False, default=1)
     sieve_size = fields.Char(string="IS Sieve Size")
     wt_retained = fields.Float(string="Wt. Retained in gms")
     percent_retained = fields.Float(string='% Retained', compute="_compute_percent_retained")
     cumulative_retained = fields.Float(string="Cum. Retained %", compute="_compute_cumulative_retained", store=True)
-    passing_percent = fields.Float(string="Passing %", compute="_compute_passing_percent")
+    passing_percent = fields.Float(string="Passing %")
 
-   
+    @api.model
+    def create(self, vals):
+        # Set the serial_no based on the existing records for the same parent
+        if vals.get('parent_id'):
+            existing_records = self.search([('parent_id', '=', vals['parent_id'])])
+            if existing_records:
+                max_serial_no = max(existing_records.mapped('serial_no'))
+                vals['serial_no'] = max_serial_no + 1
+
+        return super(SieveAnalysisLine, self).create(vals)
+
+    def _reorder_serial_numbers(self):
+        # Reorder the serial numbers based on the positions of the records in child_lines
+        records = self.sorted('id')
+        for index, record in enumerate(records):
+            record.serial_no = index + 1
+
+    def write(self, vals):
+        # Handle row deletions and adjust serial numbers
+        if 'parent_id' in vals or 'wt_retained' in vals:
+            for record in self:
+                if record.parent_id and record.parent_id == vals.get('parent_id') and 'wt_retained' in vals:
+                    record.percent_retained = vals['wt_retained'] / record.parent_id.total * 100 if record.parent_id.total else 0
+
+            new_self = super(SieveAnalysisLine, self).write(vals)
+
+            if 'wt_retained' in vals:
+                for record in self:
+                    record.parent_id._compute_total()
+
+            return new_self
+
+        return super(SieveAnalysisLine, self).write(vals)
+
+    def unlink(self):
+        # Get the parent_id before the deletion
+        parent_id = self[0].parent_id
+
+        res = super(SieveAnalysisLine, self).unlink()
+
+        if parent_id:
+            parent_id.child_lines._reorder_serial_numbers()
+
+        return res
 
 
     @api.depends('wt_retained', 'parent_id.total')
