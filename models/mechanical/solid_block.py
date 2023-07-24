e109575ee722b13e5859fb6345866d750b2fd13b
Ankitkkamble1996 <ankitrkamble9@gmail.com>
2023-07-24 08:54:44 +0000

    Solid Block

--- models/mechanical/solid_block.py
+++ models/mechanical/solid_block.py
@@ -56,6 +56,91 @@ class CompressiveStrengthLine(models.Model):
             else:
                 record.compressive_strength = 0.0
 
+
+class BlockDensity(models.Model):
+    _name = "mechanical.block.density"
+    _inherit = "lerm.eln"
+    _rec_name = "name"
+
+    name = fields.Char("Name",default="Block Density")
+    parameter_id = fields.Many2one('eln.parameters.result',string="Parameter")
+    child_lines = fields.One2many('mechanical.block.density.line','parent_id',string="Parameter")
+    average_block = fields.Float(string="Average", compute="_compute_average_block")
+
+
+    @api.depends('child_lines.block_density')
+    def _compute_average_block(self):
+        for record in self:
+            block_densities = record.child_lines.mapped('block_density')
+            if block_densities:
+                record.average_block = sum(block_densities) / len(block_densities)
+            else:
+                record.average_block = 0.0
+
+
+  
+
+    @api.model
+    def create(self, vals):
+        # import wdb;wdb.set_trace()
+        record = super(BlockDensity, self).create(vals)
+        record.parameter_id.write({'model_id':record.id})
+        return record
+
+class BlockDensityLine(models.Model):
+    _name = "mechanical.block.density.line"
+    parent_id = fields.Many2one('mechanical.block.density',string="Parent Id")
+   
+    sr_no = fields.Integer(string="Sr.No.", readonly=True, copy=False, default=1)
+    length = fields.Float(string="Length (mm)")
+    heigth = fields.Float(string="Height (mm)")
+    thickness = fields.Float(string="Thickness (mm)")
+    volume = fields.Float(string="Volume (Cm3)", compute="_compute_volume")
+    intial_wt = fields.Float(string="Initial Weight of the Specimen (kg)")
+    final_wt = fields.Float(string="Final Weight of the Specimen after Oven Dry (kg) (Constant)")
+    block_density = fields.Float(string="Block Density (kg/M³)", compute="_compute_block_density")
+
+    
+
+    @api.model
+    def create(self, vals):
+        # Set the serial_no based on the existing records for the same parent
+        if vals.get('parent_id'):
+            existing_records = self.search([('parent_id', '=', vals['parent_id'])])
+            if existing_records:
+                max_serial_no = max(existing_records.mapped('sr_no'))
+                vals['sr_no'] = max_serial_no + 1
+
+        return super(BlockDensityLine, self).create(vals)
+
+    def _reorder_serial_numbers(self):
+        # Reorder the serial numbers based on the positions of the records in child_lines
+        records = self.sorted('id')
+        for index, record in enumerate(records):
+            record.sr_no = index + 1
+
+
+   
+
+
+
+    @api.depends('length','heigth','thickness')
+    def _compute_volume(self):
+        for record in self:
+            record.volume = record.length * record.heigth * record.thickness
+
+
+    @api.depends('final_wt','volume')
+    def _compute_block_density(self):
+        for record in self:
+            if record.volume != 0:
+                record.block_density = record.final_wt / record.volume * 1000000
+            else:
+                record.block_density = 0.0
+   
+
+
+
    
 
 
