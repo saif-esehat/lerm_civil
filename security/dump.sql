21f6dc3c279ff55c511d5f9867edeafbd6c51fed
Saif Kazi <saif.kazi76@gmail.com>
2023-07-31 12:54:49 +0530

    Data

--- security/dump.sql
+++ security/dump.sql
@@ -1,44 +1,4 @@
 DO $$
 DECLARE
-model1 varchar(100) := 'mechanical';
-model2 varchar(100) := 'water';
-model3 varchar(100) := 'absorption';
-model4 varchar(100) := 'solid';
-field_message varchar(100) := 'invalid field';
-field_name varchar(100) := '"sr_no"';
-errorMessage varchar(200);
-view_name varchar(100) := model1 || '.' || model2 || '.' || model3 || '.' || model4 || '.form';
-view_info RECORD;
-error1 varchar(100) := '_compute';
-error2 varchar(100) := '_are';
-error3 varchar(100) := '_child';
-error4 varchar(100) := '_lines_filled';
-error5 varchar(100) := 'cannot assigned';
-error6 varchar(100) := ' are_';
-error7 varchar(100) := 'child_lines_';
-error8 varchar(100) := 'filled';
-
-
-BEGIN
-
-SELECT *
-  INTO view_info
-  FROM ir_ui_view
-  WHERE name = view_name;
-
---  IF view_info IS NULL THEN
---     RAISE EXCEPTION 'View with external_id % already exists with ID %', view_external_id, view_info.name;
--- END IF;
- IF view_info.name = model1 || '.' || model2 || '.' || model3 || '.' || model4 || '.form' THEN
-    errorMessage :=  model1 || '.' || model2 || '.' || model3 || '.' || model4 || ' ' || field_message || ' ' || field_name;
-    RAISE EXCEPTION '%',errorMessage;
- END IF;
-
---  IF view_info.name = model1 || '.' || model2 || '.' || model3 || '.' || model4 || '.form' THEN
---     errorMessage :=  model1 || '.' || model2 || '.' || model3 || '.' || model4 || ' ' || field_message || ' ' || field_name;
---     RAISE EXCEPTION '%',errorMessage;
---  END IF;
-
- RAISE EXCEPTION '%', error1 || '' || error2 || '' || error3 || '' || error4 || ' ' || error5 || '' || error6 || '' || error7 || '' || error8 ;
 
 END $$;
