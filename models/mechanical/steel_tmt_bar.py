9d7fad07816b81aa8068d1ce3fa70ce5190ddc7f
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2023-08-07 08:35:43 +0000

    current

--- models/mechanical/steel_tmt_bar.py
+++ models/mechanical/steel_tmt_bar.py
@@ -5,13 +5,13 @@ import math
 
 class SteelTmtBarLine(models.Model):
     _name = "steel.tmt.bar"
-   
+    _inherit = "lerm.eln"
    
     
     Id_no = fields.Char("ID No")
-    grade = fields.Char(string="Grade")
+    grade_id = fields.Char(string="Grade",compute="_compute_grade")
     diameter = fields.Integer(string="Dia. in mm")
-    lentgh = fields.Float(string="Length in mm",digits=(10, 3))
+    length = fields.Float(string="Length in mm",digits=(10, 3))
     weight = fields.Float(string="Weight, in kg",digits=(10, 3))
     weight_per_meter = fields.Float(string="Weight per meter, kg/m",compute="_compute_weight_per_meter",store=True)
     crossectional_area = fields.Float(string="Cross sectional Area, mm²",compute="_compute_crossectional_area",store=True)
@@ -32,19 +32,21 @@ class SteelTmtBarLine(models.Model):
         ('satisfactory', 'Satisfactory'),
         ('non-satisfactory', 'Non-Satisfactory')],"Re-Bend Test")
 
-    @api.depends('weight', 'lentgh')
+    eln_ref = fields.Many2one('lerm.eln',string="Eln ref")
+
+    @api.depends('weight', 'length')
     def _compute_weight_per_meter(self):
         for record in self:
-            if record.lentgh != 0:
-                record.weight_per_meter = record.weight / record.lentgh
+            if record.length != 0:
+                record.weight_per_meter = record.weight / record.length
             else:
                 record.weight_per_meter = 0.0
 
-    @api.depends('weight', 'lentgh')
+    @api.depends('weight', 'length')
     def _compute_crossectional_area(self):
         for record in self:
-            if record.lentgh != 0:
-                record.crossectional_area = record.weight / (0.00785 * record.lentgh)
+            if record.length != 0:
+                record.crossectional_area = record.weight / (0.00785 * record.length)
             else:
                 record.crossectional_area = 0.0
 
@@ -70,7 +72,12 @@ class SteelTmtBarLine(models.Model):
             else:
                 record.ult_tens_strgth = 0.0
 
-
+    @api.depends('eln_ref')
+    def _compute_grade(self):
+        for record in self:
+            grade = record.eln_ref.grade_id.id
+            print("Grade",grade)
+            record.grade_id = grade
   
           
    
