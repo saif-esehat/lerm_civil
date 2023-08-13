a7b072534fd17980a7c1a5c01e4c4feea0edf380
k1h1a1n <affykhan789678@gmail.com>
2023-08-13 11:20:47 +0000

    steel t t bar complete plus round off problem sorted

--- models/general_template.py
+++ models/general_template.py
@@ -207,7 +207,6 @@ class SteelTmtBar(models.AbstractModel):
         qr_code = qr_image_base64
         model_id = eln.model_id
         # differnt location for product based
-        print(eln.material , 'do not uncommnent this')
         model_name = eln.material.product_based_calculation[0].ir_model.name 
         if model_name:
             general_data = self.env[model_name].sudo().browse(model_id)
@@ -217,4 +216,27 @@ class SteelTmtBar(models.AbstractModel):
             'eln': eln,
             'data' : general_data,
             'qrcode': qr_code
+        }
+
+
+class SteelTmtBarDataSheet(models.AbstractModel):
+    _name = 'report.lerm_civil.steel_tmt_bar_datasheet'
+    _description = 'Steel TMT Bar DataSheet'
+    
+    @api.model
+    def _get_report_values(self, docids, data):
+        if 'active_id' in data['context']:
+            eln = self.env['lerm.eln'].sudo().search([('sample_id','=',data['context']['active_id'])])
+        else:
+            eln = self.env['lerm.eln'].sudo().browse(docids) 
+        model_id = eln.model_id
+        # differnt location for product based
+        model_name = eln.material.product_based_calculation[0].ir_model.name 
+        if model_name:
+            general_data = self.env[model_name].sudo().browse(model_id)
+        else:
+            general_data = self.env['lerm.eln'].sudo().browse(docids)
+        return {
+            'eln': eln,
+            'data' : general_data
         }
