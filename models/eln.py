fc6157d527866c203eab694acc7173729a64e6e9
root <ankitrkamble9@gmail.com>
2023-08-14 12:01:02 +0000

    Conformity

--- models/eln.py
+++ models/eln.py
@@ -379,7 +379,7 @@ class ParameteResultCalculationWizard(models.TransientModel):
             req_max = material_table.req_max
             mu_neg = record.result - record.parameter.mu_value
             mu_pos = record.result + record.parameter.mu_value
-            if req_min <= mu_neg >= req_max and req_min <= mu_pos >= req_max:
+            if req_min <= mu_neg <= req_max and req_min <= mu_pos <= req_max:
                 record.conformity_status = "pass"
             else:
                 record.conformity_status = "fail"
