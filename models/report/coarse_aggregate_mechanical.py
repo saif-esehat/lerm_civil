f5052858301141a0205e60c43d64ed2a5727b8d8
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2023-10-13 10:46:17 +0000

    pull

--- models/report/coarse_aggregate_mechanical.py
+++ models/report/coarse_aggregate_mechanical.py
@@ -8,43 +8,42 @@ from lxml import etree
 
 
 
-# class CoarseAggregateReport(models.AbstractModel):
-#     _name = 'report.lerm_civil.lerm_coarse_aggregate_report'
-#     _description = 'Cement Report'
+class CoarseAggregateReport(models.AbstractModel):
+    _name = 'report.lerm_civil.lerm_coarse_aggregate_mech_report'
+    _description = 'Coarse Aggregate Report'
     
-#     @api.model
-#     def _get_report_values(self, docids, data):
-#         # eln = self.env['lerm.eln'].sudo().browse(docids)
-#         if 'active_id' in data['context']:
-#             eln = self.env['lerm.eln'].sudo().search([('sample_id','=',data['context']['active_id'])])
-#         else:
-#             eln = self.env['lerm.eln'].sudo().browse(docids)
+    @api.model
+    def _get_report_values(self, docids, data):
+        # eln = self.env['lerm.eln'].sudo().browse(docids)
+        if 'active_id' in data['context']:
+            eln = self.env['lerm.eln'].sudo().search([('sample_id','=',data['context']['active_id'])])
+        else:
+            eln = self.env['lerm.eln'].sudo().browse(docids)
         
-#         qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-#         qr.add_data(eln.kes_no)
-#         qr.make(fit=True)
-#         qr_image = qr.make_image()
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+        qr.add_data(eln.kes_no)
+        qr.make(fit=True)
+        qr_image = qr.make_image()
 
-#         # Convert the QR code image to base64 string
-#         buffered = BytesIO()
-#         qr_image.save(buffered, format="PNG")
-#         qr_image_base64 = base64.b64encode(buffered.getvalue()).decode()
+        # Convert the QR code image to base64 string
+        buffered = BytesIO()
+        qr_image.save(buffered, format="PNG")
+        qr_image_base64 = base64.b64encode(buffered.getvalue()).decode()
 
-#         # Assign the base64 string to a field in the 'srf' object
-#         qr_code = qr_image_base64
+        # Assign the base64 string to a field in the 'srf' object
+        qr_code = qr_image_base64
             
-#         data = {
-#             "material_id":eln.material.id,
-#             "grade_id":eln.grade_id.id
-#         }
-#         model = eln.get_product_base_calc_line(data).ir_model.model
-#         coarse_data = self.env[model].search([("id","=",eln.model_id)])
-#         print(coarse_data.normal_consistency_trial1)
-#         return {
-#             'eln': eln,
-#             'coarse': coarse_data,
-#             'qrcode': qr_code
-#         }
+        data = {
+            "material_id":eln.material.id,
+            "grade_id":eln.grade_id.id
+        }
+        model = eln.get_product_base_calc_line(data).ir_model.model
+        coarse_data = self.env[model].search([("id","=",eln.model_id)])
+        return {
+            'eln': eln,
+            'data': coarse_data,
+            'qrcode': qr_code
+        }
 
 
 class CoarseAggregateDataSheet(models.AbstractModel):
