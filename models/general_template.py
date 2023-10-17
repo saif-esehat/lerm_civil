b92ddf0de2970eabcbe462efe18f44ad12a1b389
saifkazi <saif.kazi76@gmail.com>
2023-10-17 11:46:33 +0000

    pull

--- models/general_template.py
+++ models/general_template.py
@@ -618,3 +618,44 @@ class SoilDatasheet(models.AbstractModel):
         }
         
 
+class FineAggregateRep(models.AbstractModel):
+    _name = 'report.lerm_civil.fine_aggregte_mech_report'
+    _description = 'Fine'
+    
+    @api.model
+    def _get_report_values(self, docids, data):
+        # eln = self.env['lerm.eln'].sudo().browse(docids)
+        print('this is eln')
+        inreport_value = data.get('inreport', None)
+        nabl = data.get('nabl')
+        if 'active_id' in data['context']:
+            eln = self.env['lerm.eln'].sudo().search([('sample_id','=',data['context']['active_id'])])
+        else:
+            eln = self.env['lerm.eln'].sudo().browse(docids) 
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+        qr.add_data(eln.kes_no)
+        qr.make(fit=True)
+        qr_image = qr.make_image()
+
+        # Convert the QR code image to base64 string
+        buffered = BytesIO()
+        qr_image.save(buffered, format="PNG")
+        qr_image_base64 = base64.b64encode(buffered.getvalue()).decode()
+
+        # Assign the base64 string to a field in the 'srf' object
+        qr_code = qr_image_base64
+        model_id = eln.model_id
+        # differnt location for product based
+        model_name = eln.material.product_based_calculation[0].ir_model.name 
+        if model_name:
+            general_data = self.env[model_name].sudo().browse(model_id)
+        else:
+            general_data = self.env['lerm.eln'].sudo().browse(docids)
+        print(eln , 'this is eln')
+        return {
+            'eln': eln,
+            'data' : general_data,
+            'qrcode': qr_code,
+            'stamp' : inreport_value,
+            'nabl' : nabl
+        }
