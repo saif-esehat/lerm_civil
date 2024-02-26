027c42227f3b7b3d874710821bba15833fdf6269
AGJadhav98 <ajayjadhav2756@gmail.com>
2024-02-26 10:11:52 +0000

    submit mechanical

--- models/mechanical/gypsum.py
+++ models/mechanical/gypsum.py
@@ -290,6 +290,18 @@ class GypsumMechanical(models.Model):
                     record.compressive_visible = True
                     record.normal_consistency_visible = True
 
+    def open_eln_page(self):
+        # import wdb; wdb.set_trace()
+
+        return {
+                'view_mode': 'form',
+                'res_model': "lerm.eln",
+                'type': 'ir.actions.act_window',
+                'target': 'current',
+                'res_id': self.eln_ref.id,
+                
+            }                
+
     @api.model
     def create(self, vals):
         # import wdb;wdb.set_trace()
