0f4fb55e7cedd519ad31824be63091d5025404e9
Ankit Kamble <ankitrkamble@gmail.com>
2024-03-08 11:53:37 +0000

    ADD

--- models/company.py
+++ models/company.py
@@ -6,3 +6,5 @@ class LabCompany(models.Model):
     _inherit = "res.company"
     lab_certificate_no = fields.Char("Lab Certificate No .",size=6, size_min=6)
     lab_location = fields.Char("Lab Location ." , size=2, size_min=2)
+    # non_nabl = fields.Char("Non-NABL" , size=1, size_min=1)
+    # lab_location1 = fields.Char(string="Non Nabl")
