8cf8746ae6cbe69034e2da64edf1cecf391c8bb4
saifkazi <saif.kazi76@gmail.com>
2024-07-16 07:33:46 +0000

    agnes pull

--- models/mechanical/wbm.py
+++ models/mechanical/wbm.py
@@ -362,15 +362,10 @@ class WbmMechanical(models.Model):
     #     for record in self:
     #         record.plasticity_index = record.liquid_limit - record.average_plastic_moisture
 
-    @api.depends('average_plastic_moisture')
+    @api.depends('average_plastic_moisture','liquid_limit')
     def _compute_plasticity_limit(self):
         for record in self:
-            if record.average_plastic_moisture == 0.0:
-                record.plasticity_index = 'Null'
-            else:
-                plasticity_value = 46.14 - record.average_plastic_moisture
-                # Format the string representation with 2 decimal places
-                record.plasticity_index = '{:.2f}'.format(plasticity_value)
+            record.plasticity_index = record.liquid_limit - record.average_plastic_moisture
 
     # Density Relation Heavy Compaction
     density_relation_name = fields.Char("Name",default="Density Relation Using Heavy Compaction")
